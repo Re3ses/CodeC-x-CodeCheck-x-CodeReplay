132b3c6127f8409af374b64821dab8ed9907ecfa
Marco D. Mosna <mmosna.cs@gmail.com>
2024-06-13 07:48:25 +0800

    fix: allow profile generator api url

--- client/nextjs-codec/next.config.js
+++ client/nextjs-codec/next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "mir-s3-cdn-cf.behance.net",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+      },
     ],
   },
   reactStrictMode: false,
