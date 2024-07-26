ad4c72df1bdc57a61772d5d9f7e4df3d155cafbe
Marco D. Mosna <mmosna.cs+codecgithubcontrib@gmail.com>
2024-07-26 23:25:11 +0800

    style: run formatter

--- client/nextjs-codec/app/api/metrics/route.ts
+++ client/nextjs-codec/app/api/metrics/route.ts
@@ -36,6 +36,9 @@ export async function GET(request: Request) {
       created_problems_count: problemsCount.length,
     });
   } catch (e) {
-		return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-	}
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
 }
