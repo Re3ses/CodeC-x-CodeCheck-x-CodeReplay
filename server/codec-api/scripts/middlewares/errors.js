061b30b634fd90f706696e012cb9cb32e3236136
Nep <38284741+Re3ses@users.noreply.github.com>
2025-03-09 08:55:15 +0800

    minor fixes made for deployment

--- server/codec-api/scripts/middlewares/errors.js
+++ server/codec-api/scripts/middlewares/errors.js
@@ -3,7 +3,7 @@ class APIError extends Error {
         super(message)
         this.name = this.constructor.name
         this.code = code
-        
+
         Error.captureStackTrace(this, this.constructor)
     }
 }
@@ -12,7 +12,7 @@ class DocumentNotFoundError extends APIError {
     constructor(message) {
         super(message, 404)
         this.name = this.constructor.name
-        
+
         Error.captureStackTrace(this, this.constructor)
     }
 }
@@ -28,13 +28,12 @@ const wrapper = (middleware) => async (req, res, next) => {
 const handler = (err, req, res, next) => {
     if (err instanceof ReferenceError || err instanceof TypeError)
         err = new APIError(err.message, 500)
-    
+
     res.status(err.code).json({ error: err, message: err.message })
 }
 
-module.exports = {
-    wrapper,
-    handler,
-    APIError,
-    DocumentNotFoundError
-}
+module.exports.handler = (err, req, res, next) => {
+    console.error(err);  // Log error for debugging
+    const statusCode = err.status || 500;  // Default to 500 if no status
+    res.status(statusCode).json({ error: err.message || "Internal Server Error" });
+};
